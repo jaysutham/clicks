525c59a10f7e15b3fdecd64958502d814e479cbb
Jay <jaysutham@gmail.com>
2021-05-17 15:38:47 +0900

    Added automatic purchasing Added Stock Added Paperclip machine function

--- clicks/ContentView.swift
+++ clicks/ContentView.swift
@@ -11,27 +11,86 @@ struct ContentView: View {
 
     @State private var count = 0
     @State private var machineCount = 0
+    @State private var demand = 1
+    @State private var price = 1
+    @State private var money = 0
+    @State var seconds = 1
+    @State private var machinePrice = 50
+    @State private var totalClips = 0
+    
+    func sales(){
+        Timer.scheduledTimer(withTimeInterval: Double(demand), repeats: true){ timer in seconds -= 1
+            
+            let randomChance = Int.random(in: 0...10)
+            
+            if randomChance > 1{
+            if count > 0 {
+                count -= 1
+                money += price
+             }
+           }
+        }
+    }
+    
+    func machine(){
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ timer in seconds -= 1
+            count += 1
+            totalClips += 1
+     }
+    }
     
     var body: some View {
         VStack(alignment: .center){
-         Spacer()
+            Text("Total Clips: \(totalClips)")
+                .font(.title)
+            Spacer()
+            ///Money
+            HStack{
+                Text("Money: $")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Text(String(money))
+                    .font(.title)
+            }
+            //Stock Amt
             HStack{
                 Text("Stock:")
-                    .font(.largeTitle)
-                Text("\(count)").font(.largeTitle)
+                    .font(.title)
+                    .fontWeight(.bold).padding()
+                Text("\(count)").font(.title)
             }
-            
-            if self.count > 10 {
+            //Paperclip machines
+            Spacer()
+            if self.money > 10 {
                 HStack{
-                    Text("Woo!")
+                    Text("Paperclip Machine:").padding()
+                    Text("\(machineCount)")
+                    Button(action: {
+                        if (self.money - self.machinePrice) > 0 {
+                            self.money = self.money - self.machinePrice
+                            self.machineCount += 1
+                            machine()
+                        } else{
+                            Text("Not enough Money!")
+                        }
+                    }) {
+                        Text("Buy Clip Machine")
+                    }
                 }
             }
             
-            
+            //Ultimate click button
          Spacer()
-            Button(action: { self.count += 1 }) {
+            Button(action: {
+                    self.count += 10
+                    self.totalClips += 1
+                
+            }) {
                     Text("Buy Paperclip")
              }
+            Button (action: self.sales, label: {
+                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+            })
         }
        
        
@@ -41,5 +100,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }
---
+++ clicks/helper.swift
@@ -0,0 +1,14 @@
+//
+//  helper.swift
+//  clicks
+//
+//  Created by Jay Suthamjaru on 2021-05-17.
+//
+
+import Foundation
+
+func takeFromStock(){
+    let a = 10
+    let b = 20
+    print(a+b)
+}
